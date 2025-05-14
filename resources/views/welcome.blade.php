0e07eed84e44224bd428af0d057b2066773e990c
Maco System <macosystem01@gmail.com>
2025-05-14 13:44:27 -0500

    Login Creado

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -284,255 +284,32 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
         <title>Inicio - Natural Medy</title>
-        <!-- Bootstrap CSS -->
+        <link rel="icon" type="image/x-icon" href="{{ asset('images/icono.ico') }}">
+
         <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
-        <!-- jQuery -->
+
         <script src="https://code.jquery.com/jquery-1.11.1.min.js"></script>
-        <!-- Bootstrap JS -->
+
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
-        <!-- Estilos personalizados -->
-        <style>
-            /* Basic */
-            body {
-                font-family: "Poppins", sans-serif;
-                height: 100vh;
-                background: #fff;
-            }
-            
-            .wrapper {
-                display: flex;
-                align-items: center;
-                justify-content: center;
-                height: 100vh;
-            }
-            
-            /* Form Styles */
-            #formContent {
-                -webkit-border-radius: 10px 10px 10px 10px;
-                border-radius: 10px 10px 10px 10px;
-                background: #fff;
-                padding: 30px;
-                width: 20%;
-                max-width: 450px;
-                position: relative;
-                padding: 0px;
-                -webkit-box-shadow: 0 30px 60px 0 rgba(0,0,0,0.3);
-                box-shadow: 0 30px 60px 0 rgba(0,0,0,0.3);
-                text-align: center;
-            }
-            
-            #formFooter {
-                background-color: #f6f6f6;
-                border-top: 1px solid #dce8f1;
-                padding: 25px;
-                text-align: center;
-                -webkit-border-radius: 0 0 10px 10px;
-                border-radius: 0 0 10px 10px;
-            }
-            
-            /* Tabs */
-            h2.inactive {
-                color: #cccccc;
-            }
-            
-            h2.active {
-                color: #0d0d0d;
-                border-bottom: 2px solid #5fbae9;
-            }
-            
-            /* Animaciones */
-            .fadeInDown {
-                -webkit-animation-name: fadeInDown;
-                animation-name: fadeInDown;
-                -webkit-animation-duration: 1s;
-                animation-duration: 1s;
-                -webkit-animation-fill-mode: both;
-                animation-fill-mode: both;
-            }
-            
-            @-webkit-keyframes fadeInDown {
-                0% {
-                    opacity: 0;
-                    -webkit-transform: translate3d(0, -100%, 0);
-                    transform: translate3d(0, -100%, 0);
-                }
-                100% {
-                    opacity: 1;
-                    -webkit-transform: none;
-                    transform: none;
-                }
-            }
-            
-            @keyframes fadeInDown {
-                0% {
-                    opacity: 0;
-                    -webkit-transform: translate3d(0, -100%, 0);
-                    transform: translate3d(0, -100%, 0);
-                }
-                100% {
-                    opacity: 1;
-                    -webkit-transform: none;
-                    transform: none;
-                }
-            }
-            
-            /* Simple CSS3 Fade-in Animation */
-            @-webkit-keyframes fadeIn { from { opacity:0; } to { opacity:1; } }
-            @-moz-keyframes fadeIn { from { opacity:0; } to { opacity:1; } }
-            @keyframes fadeIn { from { opacity:0; } to { opacity:1; } }
-            
-            .fadeIn {
-                opacity:0;
-                -webkit-animation:fadeIn ease-in 1;
-                -moz-animation:fadeIn ease-in 1;
-                animation:fadeIn ease-in 1;
-            
-                -webkit-animation-fill-mode:forwards;
-                -moz-animation-fill-mode:forwards;
-                animation-fill-mode:forwards;
-            
-                -webkit-animation-duration:1s;
-                -moz-animation-duration:1s;
-                animation-duration:1s;
-            }
-            
-            .fadeIn.first {
-                -webkit-animation-delay: 0.4s;
-                -moz-animation-delay: 0.4s;
-                animation-delay: 0.4s;
-            }
-            
-            .fadeIn.second {
-                -webkit-animation-delay: 0.6s;
-                -moz-animation-delay: 0.6s;
-                animation-delay: 0.6s;
-            }
-            
-            .fadeIn.third {
-                -webkit-animation-delay: 0.8s;
-                -moz-animation-delay: 0.8s;
-                animation-delay: 0.8s;
-            }
-            
-            .fadeIn.fourth {
-                -webkit-animation-delay: 1s;
-                -moz-animation-delay: 1s;
-                animation-delay: 1s;
-            }
-            
-            /* Form Elements */
-            input[type=text], input[type=password] {
-                background-color: #f6f6f6;
-                border: none;
-                color: #0d0d0d;
-                padding: 15px 32px;
-                text-align: center;
-                text-decoration: none;
-                display: inline-block;
-                font-size: 16px;
-                margin: 5px;
-                width: 85%;
-                border: 2px solid #f6f6f6;
-                -webkit-transition: all 0.5s ease-in-out;
-                -moz-transition: all 0.5s ease-in-out;
-                -ms-transition: all 0.5s ease-in-out;
-                -o-transition: all 0.5s ease-in-out;
-                transition: all 0.5s ease-in-out;
-                -webkit-border-radius: 5px 5px 5px 5px;
-                border-radius: 5px 5px 5px 5px;
-            }
-            
-            input[type=text]:focus, input[type=password]:focus {
-                background-color: #fff;
-                border-bottom: 2px solid #5fbae9;
-            }
-            
-            input[type=text]:placeholder, input[type=password]:placeholder {
-                color: #cccccc;
-            }
-            
-            input[type=submit] {
-                background-color: #56baed;
-                border: none;
-                color: white;
-                padding: 15px 80px;
-                text-align: center;
-                text-decoration: none;
-                display: inline-block;
-                text-transform: uppercase;
-                font-size: 13px;
-                -webkit-box-shadow: 0 10px 30px 0 rgba(95,186,233,0.4);
-                box-shadow: 0 10px 30px 0 rgba(95,186,233,0.4);
-                -webkit-border-radius: 5px 5px 5px 5px;
-                border-radius: 5px 5px 5px 5px;
-                margin: 5px 20px 40px 20px;
-                -webkit-transition: all 0.3s ease-in-out;
-                -moz-transition: all 0.3s ease-in-out;
-                -ms-transition: all 0.3s ease-in-out;
-                -o-transition: all 0.3s ease-in-out;
-                transition: all 0.3s ease-in-out;
-            }
-            
-            input[type=submit]:hover {
-                background-color: #39ace7;
-            }
-            
-            input[type=submit]:active {
-                -moz-transform: scale(0.95);
-                -webkit-transform: scale(0.95);
-                -o-transform: scale(0.95);
-                -ms-transform: scale(0.95);
-                transform: scale(0.95);
-            }
-            
-            /* Links */
-            .underlineHover {
-                color: #0d0d0d;
-                text-decoration: none;
-                display: inline-block;
-                position: relative;
-            }
-            
-            .underlineHover:after {
-                content: '';
-                position: absolute;
-                width: 100%;
-                transform: scaleX(0);
-                height: 2px;
-                bottom: 0;
-                left: 0;
-                background-color: #56baed;
-                transform-origin: bottom right;
-                transition: transform 0.25s ease-out;
-            }
-            
-            .underlineHover:hover:after {
-                transform: scaleX(1);
-                transform-origin: bottom left;
-            }
-            
-            /* Icono */
-            #icon {
-                width: 60%;
-                padding: 20px 0;
-            }
-        </style>
+
+        <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+
     </head>
     <body>
         <div class="wrapper fadeInDown">
             <div id="formContent">
-                <!-- Icon -->
+
                 <div class="fadeIn first">
-                    <img src="https://acortar.link/YmnSBB" id="icon" alt="User Icon" />
+                    <img src="images/logo.png" id="icon" alt="User Icon" />
                     <h3>Registra tus Datos</h3>
                 </div>
 
-                <!-- Login Form -->
                 <form>
-                    <input type="text" id="login" class="fadeIn second" name="login" placeholder="Usuario">
-                    <input type="password" id="password" class="fadeIn third" name="password" placeholder="Contraseña">
-                    <input type="submit" class="fadeIn fourth" value="Iniciar Sesión">
+                    <input type="number" id="login" class="form-input" name="login" placeholder="Código ID">
+                    <input type="password" id="password" class="form-input" name="password" placeholder="Contraseña">
+                    <input type="submit" class="form-button" value="Iniciar Sesión">
                 </form>
+
             </div>
         </div>
     </body>
