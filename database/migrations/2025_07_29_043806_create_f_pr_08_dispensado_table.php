63fde292a4d187b3e72e97c21b5feba8772e2490
Maco System <macosystem01@gmail.com>
2025-07-28 23:51:24 -0500

    Se realizó el F-PR-08_dispensado y se migró con éxito

---
+++ database/migrations/2025_07_29_043806_create_f_pr_08_dispensado_table.php
@@ -0,0 +1,58 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('f_pr_08_dispensado', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+
+            $table->string('codigo');
+            $table->string('producto');
+            $table->string('presentacion');
+            $table->string('lote');
+            $table->string('registro_sanitario');
+            $table->string('codigo_barras');
+            $table->decimal('tamaño', 10, 2);
+            $table->timestamp('fecha_vencimiento')->nullable();
+            $table->string('unidades_lote');
+            // PROCESO DISPENSADO
+            $table->timestamp('hora_inicio')->nullable();
+            $table->timestamp('hora_fin')->nullable();
+            $table->integer('numero_lote');
+            $table->integer('codigo_materia_prima');
+            $table->string('materia_prima_requerida');
+            $table->decimal('porcentaje', 5, 2);
+            $table->string('cantidad_estandar');
+            $table->string('unidad');
+            $table->timestamp('fecha_pesaje')->nullable();
+            $table->decimal('cantidad_pesada', 10, 2);
+            $table->string('pesado_por');
+            $table->timestamp('fecha_verificacion')->nullable();
+            $table->decimal('peso_corregido', 10, 2);
+            $table->string('verificado_por');
+
+
+            // METADATOS
+            $table->timestamp('fecha_contenida')->nullable();
+            $table->timestamp('aproved_at')->nullable();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('f_pr_08');
+    }
+};
