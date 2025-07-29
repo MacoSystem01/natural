7fd3fd0709bc3df1a0d06821d12f3d6e1225126a
Maco System <macosystem01@gmail.com>
2025-07-29 01:04:00 -0500

    Se realizó el F-PR-08_acondicionamiento y se migró con éxito

--- database/migrations/2025_07_29_054936_create_f_pr_08_acondicionamiento_table.php
+++ database/migrations/2025_07_29_054936_create_f_pr_08_acondicionamiento_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('f_pr_08_envasado', function (Blueprint $table) {
+        Schema::create('f_pr_08_acondicionamiento', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
 
@@ -24,7 +24,7 @@ return new class extends Migration
             $table->decimal('tamaño', 10, 2);
             $table->timestamp('fecha_vencimiento')->nullable();
             $table->string('unidades_lote');
-            // PROCESO ENVASADO
+            // PROCESO DE ACONDICIONAMIENTO
             $table->integer('numero_lote');
             $table->string('codigo_material');
             $table->string('material_requerido');
@@ -34,14 +34,25 @@ return new class extends Migration
             $table->integer('entrega_material_orden_cantidad');
             $table->string('revisado_por');
             $table->timestamp('revisado_fecha')->nullable();
-            // ENVASADO
-            $table->timestamp('fecha_envasado')->nullable();
-            $table->string('personal_envasado_nombre');
-            $table->integer('personal_envasado_codigo');
-            $table->timestamp('hora_inicio_envasado')->nullable();
-            $table->timestamp('hora_fin_envasado')->nullable();
+            // ACONDICIONAMIENTO
+            $table->timestamp('fecha_acondicionamiento')->nullable();
+            $table->string('personal_acondicionamiento_nombre');
+            $table->integer('personal_acondicionamiento_codigo');
+            $table->timestamp('hora_inicio_acondicionamiento')->nullable();
+            $table->timestamp('hora_fin_acondicionamiento')->nullable();
             $table->string('cantidad_un');
             $table->string('observaciones_generales');
+            // FIRMA
+            $table->string('emitido_produccion_nombre');
+            $table->timestamp('emitido_produccion_fecha')->nullable();
+            $table->string('gestion_calidad_nombre');
+            $table->timestamp('gestion_calidad_fecha')->nullable();
+            $table->string('descargo_inventario_nombre');
+            $table->timestamp('descargo_inventario_fecha')->nullable();
+            // METADATOS
+            $table->timestamp('fecha_contenida')->nullable();
+            $table->timestamp('aproved_at')->nullable();
+            $table->softDeletes();
 
         });
     }
@@ -51,6 +62,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('f_pr_08_envasado');
+        Schema::dropIfExists('f_pr_08_acondicionamiento');
     }
 };
